d9226b4a2fdf98b21a01445c4c6d54ff045cf66d
Renaldas Bendikas <renaldasb1999@gmail.com>
2023-12-28 10:56:07 +0000

    add scroll to read more btn

--- assets/js/home.js
+++ assets/js/home.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
 const openModal = function (e) {
     e.preventDefault();
@@ -29,4 +31,18 @@ document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
     }
+});
+
+btnScrollTo.addEventListener('click', function (e) {
+    const s1coords = section1.getBoundingClientRect();
+
+    // window.scrollTo(s1coords.left + window.pageXOffSet, s1coords.top + window.pageYOffset);
+
+    // window.scrollTo({
+    //     left: s1coords.left + window.pageXOffSet,
+    //     top: s1coords.top + window.pageYOffset,
+    //     behavior: 'smooth'
+    // });
+
+    section1.scrollIntoView({ behavior: 'smooth' });
 });
