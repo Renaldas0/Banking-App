2a6017d56a551a49a036777fbe9cf8316c8b9cdf
Renaldas Bendikas <97538312+Renaldas0@users.noreply.github.com>
2024-01-16 23:37:15 +0000

    add sticky nav and section load

--- assets/js/home.js
+++ assets/js/home.js
@@ -88,12 +88,41 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 // Sticky Navgiation
-const initialCoords = section1.getBoundingClientRect();
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > initialCoords.top) {
-        nav.classList.add('sticky');
-    } else {
-        nav.classList.remove('sticky');
-    };
+function stickyNav(entries) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) nav.classList.add('sticky')
+    else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`
+});
+
+headerObserver.observe(header);
+
+// Show Sections on scroll
+const allSections = document.querySelectorAll('.section');
+
+function showSection(entries, observer) {
+    const [entry] = entries;
+
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+};
+
+const sectionObserver = new IntersectionObserver(showSection, {
+    root: null,
+    threshold: 0.12
+});
+
+allSections.forEach(section => {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
 });
