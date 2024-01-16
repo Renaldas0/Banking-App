c7ec90e67859af0dcf61b804a3973474dd6ca425
Renaldas Bendikas <renaldasb1999@gmail.com>
2024-01-16 13:23:20 +0000

    add sticky nav

--- assets/js/home.js
+++ assets/js/home.js
@@ -86,3 +86,14 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky Navgiation
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > initialCoords.top) {
+        nav.classList.add('sticky');
+    } else {
+        nav.classList.remove('sticky');
+    };
+});
